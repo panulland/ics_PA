9a7e13af202f9d496678a21a111bfd5794fd6ba9
monitor <tracer@njuics.org>
2018-09-20 00:35:01 +0800

    monitor test

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -64,7 +64,8 @@ uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size) {
 	return res & (0xFFFFFFFF >> (32 - data_size));
 }
 
-void set_CF_adc(uint32_t result, uint32_t src, uint32_t dest; size_t data_size){
+void set_CF_adc(uint32_t result, uint32_t src, uint32_t dest, size_t data_size){
+	cpu.eflags.CF=0;
 	result = sign_ext(result&(0xFFFFFFFFF>>(32-data_size)),data_size);
 	src = sign_ext(result&(0xFFFFFFFF>>(32-data_size)),data_size);
 	dest = sign_ext(result&(0xFFFFFFFF>>(32-data_size)),data_size);
