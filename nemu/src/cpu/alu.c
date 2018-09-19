201ebb260f2ca525828b10f06f602c26cb905c73
monitor <tracer@njuics.org>
2018-09-20 00:32:01 +0800

    monitor test

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -1,7 +1,7 @@
 #include "cpu/cpu.h"
 
 void set_CF_add(uint32_t result,uint32_t src,size_t data_size){
-	result=sign_ext(result&(0xFFFFFFFF>>(32-data_size)),data_size);
+	result=sign_ext(result & (0xFFFFFFFF>>(32-data_size)),data_size);
 	src=sign_ext(src & (0xFFFFFFFF>>(32-data_size)),data_size);
 	cpu.eflags.CF=(result<src);
 }
@@ -64,11 +64,18 @@ uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size) {
 	return res & (0xFFFFFFFF >> (32 - data_size));
 }
 
+void set_CF_adc(uint32_t result, uint32_t src, uint32_t dest; size_t data_size){
+	result = sign_ext(result&(0xFFFFFFFFF>>(32-data_size)),data_size);
+	src = sign_ext(result&(0xFFFFFFFF>>(32-data_size)),data_size);
+	dest = sign_ext(result&(0xFFFFFFFF>>(32-data_size)),data_size);
+	cpu.eflags.CF=(result<src||result<dest);
+}
+
 uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size) {
 	uint32_t res = 0;
 	res = dest + src + cpu.eflags.CF;
 
-	set_CF_add(res, src, data_size);
+	set_CF_adc(res, src, data_size);
 	set_PF(res);
 	set_ZF(res,data_size);
 	set_SF(res,data_size);
