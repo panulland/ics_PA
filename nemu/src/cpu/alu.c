1d68fce4e2b553773fdaacb6201fda934c6e49ec
tracer-pa2018-fall <tracer@njuics.org>
2018-09-20 19:07:41 +0800

    >  nemu 171860516 cpl Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  19:07:41 up  3:27,  1 user,  load average: 0.11, 0.17, 0.07 3e1629934e5cdd1f9bb03ef4171635f387e231af

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -66,7 +66,7 @@ uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size) {
 
 void set_CF_adc(uint32_t result, uint32_t src, size_t data_size){
 	result = sign_ext(result&(0xFFFFFFFF>>(32-data_size)),data_size);
-	src = sign_ext(result&(0xFFFFFFFF>>(32-data_size)),data_size);
+	src = sign_ext(src&(0xFFFFFFFF>>(32-data_size)),data_size);
 	if(cpu.eflags.CF)
 		cpu.eflags.CF = result<=src;
 	else
