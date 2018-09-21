235804224cc19f39029fcfe279c6f8983727270f
monitor <tracer@njuics.org>
2018-09-22 01:48:01 +0800

    monitor test

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -146,14 +146,10 @@ uint32_t alu_sbb(uint32_t src, uint32_t dest, size_t data_size) {
 
 
 uint64_t alu_mul(uint32_t src, uint32_t dest, size_t data_size) {
-	uint64_t src0=sign_ext_64(src&(0xFFFFFFFF>>(32-data_size)),data_size);
-	uint64_t dest0=sign_ext_64(dest&(0xFFFFFFFF>>(32-data_size)),data_size);
-	printf("%llx\n",src0);
-	printf("%llx\n",dest0);
-	uint64_t res = 0;
+	uint32_t res = 0;
 	printf("%x\n",src);
 	res = dest * src;
-	printf("%llx\n",res);
+	printf("%x\n",res);
 	uint64_t r=res;
 
 	switch(data_size){
