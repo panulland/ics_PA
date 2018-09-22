e0997df1dd72a8db3eba7f0d99b83be034c797f0
tracer-pa2018-fall <tracer@njuics.org>
2018-09-22 09:27:28 +0800

    >  nemu 171860516 cpl Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  09:27:28 up 13:37,  1 user,  load average: 0.09, 0.08, 0.08 a8672637878945d0152fd0ea2545f231b3af4e1e

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -177,7 +177,7 @@ int64_t alu_imul(int32_t src, int32_t dest, size_t data_size) {
 	int64_t dest0 = sign_ext_64(dest,data_size);
 	int64_t res = 0;
 	res = src0 * dest0;
-	printf("%llx\n",res);
+	printf("%llx\n======\n",res);
 
 	if((s&&!d)||(!s&&d))
 		return res | (0xFFFFFFFFFFFFFFFF << (2*data_size - 1));
