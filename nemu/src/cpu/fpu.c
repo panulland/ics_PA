97a50f53cb145818b6931fb1561f4d25a686246c
tracer-pa2018-fall <tracer@njuics.org>
2018-09-22 12:51:53 +0800

    >  nemu 171860516 cpl Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  12:51:53 up 16:27,  1 user,  load average: 0.03, 0.06, 0.08 5939d587e26eb1d13261166da644d26159a7a134

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -71,7 +71,7 @@ inline uint32_t internal_normalize(uint32_t sign, int32_t exp, uint64_t sig_grs)
 			if(sig_grs % 2)
 				sig_grs ++;
 		}
-		if((sig_grs >> 23) > 1){
+		if((sig_grs >> 23) > 0){
 			sig_grs = sig_grs << 3;
 			return internal_normalize(sign,exp,sig_grs);
 		}
