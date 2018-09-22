3fb27840850e7fc301d0cc4cb398a7198cf99ee2
monitor <tracer@njuics.org>
2018-09-22 15:24:01 +0800

    monitor test

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -243,10 +243,7 @@ uint32_t internal_float_mul(uint32_t b, uint32_t a) {
 
 	/* TODO: exp_res = ? leave space for GRS bits. */
 	exp_res = fa.exponent + fb.exponent - 127;
-	printf("=====%llx=====%llx====\n",sig_a,sig_b);
-	printf("++++++%x++++%x+++%llx+++++\n",b,a,sig_res);
 	sig_res = sig_res << 3;
-	printf("******%x****%llx***\n",exp_res,sig_res);
 	return internal_normalize(f.sign, exp_res, sig_res);
 }
 
