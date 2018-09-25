6745e72fbfaec55d68e6e7427a3536d0e41fa62c
tracer-pa2018-fall <tracer@njuics.org>
2018-09-25 22:54:17 +0800

    >  nemu 171860516 cpl Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  22:54:17 up 1 day, 29 min,  1 user,  load average: 0.13, 0.06, 0.01 57b1edbcc4fcef9d8c485bbc3eb423d3460fbfed

--- nemu/src/cpu/test/fpu_test.c
+++ nemu/src/cpu/test/fpu_test.c
@@ -131,7 +131,7 @@ void fpu_test_mul() {
 			//printf("res     sign %x, exp %x, %d, sig %x\n", res.sign, res.exponent, res.exponent, res.significand);
 			//printf("res_fpu sign %x, exp %x, %d, sig %x\n", res_fpu.sign, res_fpu.exponent, res_fpu.exponent, res_fpu.significand);
 if(res_fpu.val==0){
-printf("%x %x\n",a,b)
+printf("%x %x\n",a.val,b.val)
 }
 			assert(res_fpu.val == res.val);
 		}
