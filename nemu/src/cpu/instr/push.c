4fc22d5ef164fe2b71be91e1272ec97fd72262e0
tracer-pa2018-fall <tracer@njuics.org>
2018-10-16 23:50:39 +0800

    >  nemu 171860516 cpl Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  23:50:39 up 1 day, 14:58,  1 user,  load average: 0.15, 0.14, 0.09 68ba452aa5b7b5b1251f041c3667f34a8a276af

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -1,13 +1,13 @@
 #include "cpu/instr.h"
 
-static void instr_execute_1op() {
+/*static void instr_execute_1op() {
 	opr_dest.type = OPR_REG;
 	opr_dest.addr = 0x4;
 	operand_read(&opr_src);
 	//cpu.esp=cpu.esp-4;
 	opr_dest.val = opr_src.val;
 	operand_write(&opr_dest);
-}
+}*/
 
 //make_instr_impl_1op(push,r,v)
 
