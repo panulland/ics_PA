5ee40561623ab4bbe80d38e5078fce4666c48261
tracer-pa2018-fall <tracer@njuics.org>
2018-10-17 00:59:33 +0800

    >  nemu 171860516 cpl Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  00:59:33 up 38 min,  1 user,  load average: 0.09, 0.11, 0.09 cdc0141a5b68826afca404f45477ac177f1d9d8e

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -7,9 +7,13 @@ make_instr_func(call_near) {
 	rel.data_size = data_size;
 	rel.addr = eip + 1;
 
+	operand_read(&rel);
+
 	int offset = sign_ext(rel.val,data_size);
 	print_asm_1("call","",2,&rel);
 
-	//cpu.eip += offset;
+
+
+	cpu.eip += offset;
 	return 1 + data_size / 8;
 }
