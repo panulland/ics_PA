29d75b29718bedbd2733166055b5dd15176b6f6c
tracer-pa2018-fall <tracer@njuics.org>
2018-10-18 01:14:32 +0800

    >  nemu 171860516 cpl Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  01:14:32 up  7:53,  1 user,  load average: 0.12, 0.06, 0.01 8cc9cd00a4e139b29257d3e6ac916e67f9821f8a

--- nemu/src/cpu/instr/lea.c
+++ nemu/src/cpu/instr/lea.c
@@ -6,12 +6,12 @@ make_instr_func(lea) {
 	m.data_size=r.data_size=data_size;
 
 	int len=1;
-	len += modrm_r_rm(eip+1,&m,&r);
+	len += modrm_r_rm(eip+1,&r,&m);
 
 	r.val=m.addr;
 	operand_write(&r);
 
-	print_asm_2("lea","",2,&m,&r);
+	print_asm_2("lea","",3,&m,&r);
 
 	return len;
 }
