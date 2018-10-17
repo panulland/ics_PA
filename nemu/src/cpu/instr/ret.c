8d92b4590abf51332b68e76ef5b46ddd0dcee0ad
tracer-pa2018-fall <tracer@njuics.org>
2018-10-17 20:57:27 +0800

    >  nemu 171860516 cpl Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  20:57:27 up  3:36,  1 user,  load average: 0.03, 0.08, 0.11 8305fff3fe0b7dfbf3c813b661ea0a3e3f51ac78

--- nemu/src/cpu/instr/ret.c
+++ nemu/src/cpu/instr/ret.c
@@ -1,12 +1,16 @@
 #include "cpu/instr.h"
 
 make_instr_func(ret_near) {
+	OPERAND rel;
 	OPERAND stack;
 	stack.type=OPR_REG;
 	stack.data_size=data_size;
 	stack.addr=0x4;
+	rel.data_size=data_size;
 	operand_read(&stack);
-	cpu.eip=stack.val;
+	rel.addr=stack.val;
+	operand_read(&rel);
+	cpu.eip=rel.val;
 	cpu.esp+=4;
 
 	print_asm_0("ret","",1);
