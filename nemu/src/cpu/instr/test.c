1d01d163aa2ad49cc80504759f21bb5209b2aa91
tracer-pa2018-fall <tracer@njuics.org>
2018-10-17 20:42:51 +0800

    >  nemu 171860516 cpl Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  20:42:51 up  3:22,  1 user,  load average: 0.05, 0.08, 0.08 e46f7f3b8b8f1d7cb1aa942baecb88094aed7e3e

--- nemu/src/cpu/instr/test.c
+++ nemu/src/cpu/instr/test.c
@@ -1,7 +1,7 @@
 #include "cpu/instr.h"
 
 static void instr_execute_2op() {
-	operand_read(&opr_read);
+	operand_read(&opr_src);
 	opr_dest.val = opr_src.val & opr_dest.val;
 	cpu.eflags.CF = 0;
 	cpu.eflags.OF = 0;
