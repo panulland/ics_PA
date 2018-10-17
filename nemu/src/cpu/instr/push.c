183ecd4f75ec21207732507676e348868ffeb701
monitor <tracer@njuics.org>
2018-10-17 20:47:01 +0800

    monitor test

--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -5,8 +5,8 @@ static void instr_execute_1op() {
 	opr_dest.addr = 0x4;
 	operand_read(&opr_src);
 	cpu.esp=cpu.esp-4;
-	opr_dest.val = opr_src.val;
-	operand_write(&opr_dest);
+	//opr_dest.val = opr_src.val;
+	//operand_write(&opr_dest);
 }
 
 make_instr_impl_1op(push,r,v)
