a04be44120cd509373417ec52a0ab8d1601ec9d2
tracer-pa2018-fall <tracer@njuics.org>
2018-10-18 00:00:25 +0800

    >  nemu 171860516 cpl Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  00:00:25 up  6:39,  1 user,  load average: 0.06, 0.13, 0.09 c4e6e63861e4fedf7381029e9eda8caecd371b0f

--- nemu/include/cpu/instr/cmp.h
+++ nemu/include/cpu/instr/cmp.h
@@ -2,10 +2,8 @@
 #define __INSTR_CMP_H_
 
 make_instr_func(cmp_i2rm_bv);
-make_instr_func(cmp_r2rm_v);
 make_instr_func(cmp_i2rm_v);
 make_instr_func(cmp_i2rm_b);
-make_instr_func(cmp_i2rm_bv);
 make_instr_func(cmp_r2rm_b);
 make_instr_func(cmp_r2rm_v);
 make_instr_func(cmp_rm2r_b);
