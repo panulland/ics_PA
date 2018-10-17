c0ce9170971ac2653b168620e06b10285b7405e3
tracer-pa2018-fall <tracer@njuics.org>
2018-10-17 18:59:28 +0800

    >  nemu 171860516 cpl Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  18:59:28 up  1:38,  1 user,  load average: 0.04, 0.08, 0.08 b90ca52f7137872b12efaa11715409c519d03431

---
+++ nemu/src/cpu/instr/cmp.c
@@ -0,0 +1,9 @@
+#include "cpu/instr.h"
+#include "cpu/alu.h"
+
+static void instr_execute_2op() {
+	operand_read (&opr_src);
+	unit32_t temp = alu_sub(opr_src.val,opr_dest.val,data_size);
+}
+
+make_instr_impl_2op(cmp,i,rm,bv)
