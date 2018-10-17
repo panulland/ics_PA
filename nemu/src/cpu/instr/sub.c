514c31e1ac0756c882dbc17c20ee296eebe653f3
monitor <tracer@njuics.org>
2018-10-17 22:42:01 +0800

    monitor test

--- nemu/src/cpu/instr/sub.c
+++ nemu/src/cpu/instr/sub.c
@@ -2,8 +2,10 @@
 
 static void instr_execute_2op() {
 	operand_read (&opr_src);
-	opr_dest.val=opr_dest.val-opr_src.val;
+	opr_dest.val=alu_sub(opr_dest.val,opr_src.val,data_size);
 	operand_write(&opr_dest);
 }
 
+make_instr_impl_2op(sub,i,rm,b)
+make_instr_impl_2op(sub,i,rm,v)
 make_instr_impl_2op(sub,i,rm,bv)
