8bfc35c2c38933bda804462e1c65ddcbf376421e
monitor <tracer@njuics.org>
2018-10-17 22:05:01 +0800

    monitor test

--- nemu/include/cpu/instr/jcc.h
+++ nemu/include/cpu/instr/jcc.h
@@ -1,6 +1,20 @@
 #ifndef __INSTR_JCC_H_
 #define __INSTR_JCC_H_
 
+make_instr_func(jo_short_);
+make_instr_func(jno_short_);
+make_instr_func(jb_short_);
+make_instr_func(jae_short_);
+make_instr_func(jne_short_);
+make_instr_func(ja_short_);
+make_instr_func(js_short_);
+make_instr_func(jns_short_);
+make_instr_func(jp_short_);
+make_instr_func(jnp_short_);
+make_instr_func(jl_short_);
+make_instr_func(jge_short_);
+make_instr_func(jle_short_);
+make_instr_func(jg_short_);
 make_instr_func(jna_short_);
 make_instr_func(jbe_short_);
 make_instr_func(jne_short_);
