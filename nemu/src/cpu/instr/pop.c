eb0323f2ecf3ec9cb85f932e8dd09e538a2c6527
monitor <tracer@njuics.org>
2018-10-18 09:37:02 +0800

    monitor test

--- nemu/src/cpu/instr/pop.c
+++ nemu/src/cpu/instr/pop.c
@@ -6,4 +6,8 @@ static void instr_execute_1op() {
 	opr_dest.data_size=data_size;
 	operand_read(&opr_dest);
 	cpu.esp += 4;
+	opr_src.val=opr_dest.val;
+	operand_read(&opr_src);
 }
+
+make_instr_impl_1op(pop,r,v)
