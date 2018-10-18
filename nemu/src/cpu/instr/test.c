3c2bcb315a6bc5519d0d281f256e89da4c202ddb
tracer-pa2018-fall <tracer@njuics.org>
2018-10-18 23:03:13 +0800

    >  nemu 171860516 cpl Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  23:03:13 up 13:22,  1 user,  load average: 0.06, 0.09, 0.07 995a3b86584ebf70caeefbb2c626b67cafd449c1

--- nemu/src/cpu/instr/test.c
+++ nemu/src/cpu/instr/test.c
@@ -4,10 +4,8 @@ static void instr_execute_2op() {
 	operand_read(&opr_src);
 	operand_read(&opr_dest);
 	uint32_t tmp = opr_dest.val;	
-	opr_dest.val = alu_and (opr_dest.val,opr_src.val,data_size);
+	opr_dest.val = alu_and (opr_dest.val,opr_src.val,opr_dest.data_size);
 	opr_dest.val = tmp;
-	cpu.eflags.CF = 0;
-	cpu.eflags.OF = 0;
 }
 
 make_instr_impl_2op(test,r,rm,v)
