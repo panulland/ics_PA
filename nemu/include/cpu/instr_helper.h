499ed0568abe861634868c4ec0ae41841067dd18
tracer-pa2018-fall <tracer@njuics.org>
2018-10-19 00:31:19 +0800

    >  nemu 171860516 cpl Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  00:31:19 up 14:50,  1 user,  load average: 0.06, 0.04, 0.01 e00973f0f7da63462744a970ab544780bcfed9dc

--- nemu/include/cpu/instr_helper.h
+++ nemu/include/cpu/instr_helper.h
@@ -71,10 +71,11 @@ void print_asm_3(char * instr, char * suffix, uint8_t len, OPERAND * opr_1, OPER
 #define decode_data_size_l opr_src.data_size = opr_dest.data_size = 32;
 #define decode_data_size_v opr_src.data_size = opr_dest.data_size = data_size;
 #define decode_data_size_bv \
-       	opr_src.data_size = 8; opr_dest.data_size = data_size; \
+       	opr_src.data_size = data_size; opr_dest.data_size = data_size; \
 	operand_read(&opr_src); \
 	opr_src.val = sign_ext(opr_src.val,8)&(0xFFFFFFFF>>(32-data_size)); \
-	operand_write(&opr_src);
+	operand_write(&opr_src); \
+	opr_src.data_size = 8;
 #define decode_data_size_short_ opr_src.data_size = opr_dest.data_size = 8;
 #define decode_data_size_near opr_src.data_size = opr_dest.data_size = 32;
 
