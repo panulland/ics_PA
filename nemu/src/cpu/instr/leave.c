558d4167f4e3e6464560d985b1650b9dbe8cfdbf
tracer-pa2018-fall <tracer@njuics.org>
2018-10-18 20:33:31 +0800

    >  nemu 171860516 cpl Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  20:33:31 up 11:19,  1 user,  load average: 0.28, 0.09, 0.03 b9bed78ba2a34f8472317020dc2acf45bc19246c

---
+++ nemu/src/cpu/instr/leave.c
@@ -0,0 +1,28 @@
+#include "cpu/instr.h"
+
+make_instr_func(leave) {
+	OPERAND r;
+	r.data_size=data_size;
+	r.type=OPR_REG;
+	r.addr=0x5;
+	operand_read(&r);
+	OPERAND r2;
+	r2.data_size=data_size;
+	r2.type=OPR_REG;
+	r2.addr=0x4;
+	r2.val=r.val;
+	operand_write(&r2);
+
+	OPERAND m;
+	m.data_size=data_size;
+	m.type=OPR_MEM;
+	m.addr=cpu.esp;
+	operand_read(&m);
+	cpu.esp+=4;
+	r.val=m.val;
+	operand_read(&r);
+
+	print_asm_0("leave","",1);
+	return 1;
+}
+
