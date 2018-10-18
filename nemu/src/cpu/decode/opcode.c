c3503d278d790b87bc17e84e77fbae8dc1209fab
tracer-pa2018-fall <tracer@njuics.org>
2018-10-18 09:30:50 +0800

    >  nemu 171860516 cpl Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  09:30:50 up  9:05,  1 user,  load average: 0.06, 0.08, 0.05 48a6b654c83a5793bffd46e2fc3dce08fc2b56a3

--- nemu/src/cpu/decode/opcode.c
+++ nemu/src/cpu/decode/opcode.c
@@ -21,8 +21,8 @@ instr_func opcode_entry[256] = {
 /* 0x44 - 0x47*/	inc_r_v, inc_r_v, inc_r_v, inc_r_v,
 /* 0x48 - 0x4b*/	dec_r_v, dec_r_v, dec_r_v, dec_r_v,
 /* 0x4c - 0x4f*/	dec_r_v, dec_r_v, dec_r_v, dec_r_v,
-/* 0x50 - 0x53*/	push_r_v, __ref_push_r_v, push_r_v, __ref_push_r_v,
-/* 0x54 - 0x57*/	push_r_v, __ref_push_r_v, __ref_push_r_v, __ref_push_r_v,
+/* 0x50 - 0x53*/	push_r_v, push_r_v, push_r_v, push_r_v,
+/* 0x54 - 0x57*/	push_r_v, push_r_v, push_r_v, push_r_v,
 /* 0x58 - 0x5b*/	inv, inv, inv, inv,
 /* 0x5c - 0x5f*/	inv, inv, inv, inv,
 /* 0x60 - 0x63*/	inv, inv, inv, inv,
@@ -113,7 +113,7 @@ instr_func group_3_v_entry[8] =
 
 /* 0xff */
 instr_func group_5_indirect_entry[8] =
-{inc_rm_v, dec_rm_v, /*call_near_indirect*/inv, inv,/* jmp_near_indirect*/inv, inv, __ref_push_rm_v, inv};
+{inc_rm_v, dec_rm_v, /*call_near_indirect*/inv, inv,/* jmp_near_indirect*/inv, inv, push_rm_v, inv};
 
 instr_func group_7_entry[8] = 
 {inv, inv, inv, inv, inv, inv, inv, inv};
--- nemu/src/cpu/instr/push.c
+++ nemu/src/cpu/instr/push.c
@@ -1,11 +1,12 @@
 #include "cpu/instr.h"
 
 static void instr_execute_1op() {
+	operand_read(&opr_src);
 	opr_dest.type = OPR_MEM;
 	cpu.esp -= 4;
 	opr_dest.addr = cpu.esp;
 	opr_dest.data_size=data_size;
-	opr_dest.val = cpu.eip;
+	opr_dest.val = opr_src.val;
 	operand_write(&opr_dest);
 }
 
