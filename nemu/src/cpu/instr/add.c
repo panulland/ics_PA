1715aeed72f4fad899535d999cbab43102de37a4
tracer-pa2018-fall <tracer@njuics.org>
2018-10-19 00:43:56 +0800

    >  nemu 171860516 cpl Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  00:43:56 up 15:03,  1 user,  load average: 0.05, 0.07, 0.02 6664540bcc562a109b49e649a3a04623346d9685

--- nemu/src/cpu/instr/add.c
+++ nemu/src/cpu/instr/add.c
@@ -3,6 +3,7 @@
 static void instr_execute_2op() {
 	operand_read(&opr_src);
 	operand_read(&opr_dest);
+	printf("=====\n");
 	if(opr_src.data_size == 8 && opr_src.data_size == data_size)
 	{
 		opr_src.val = sign_ext(opr_src.val,8) & (0xFFFFFFFF >> (32 - data_size));
