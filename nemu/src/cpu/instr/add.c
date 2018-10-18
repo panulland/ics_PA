5cc4a49c42f64c22082350bcc91fd063f7939ab9
tracer-pa2018-fall <tracer@njuics.org>
2018-10-18 23:44:43 +0800

    >  nemu 171860516 cpl Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  23:44:43 up 14:04,  1 user,  load average: 0.00, 0.03, 0.00 bd74ed0cae5bb93f08ce031920edfeca0cc3c

--- nemu/src/cpu/instr/add.c
+++ nemu/src/cpu/instr/add.c
@@ -3,7 +3,7 @@
 static void instr_execute_2op() {
 	operand_read(&opr_src);
 	operand_read(&opr_dest);
-	if(opr_src.type==OPR_IMM&&opr_src.data_size=8)
+	if(opr_src.type==OPR_IMM&&opr_src.data_size==8)
 		opr_src.val=sign_ext(opr_src.val,8);
 	opr_dest.val = alu_add(opr_src.val,opr_dest.val,opr_dest.data_size);
 	operand_write(&opr_dest);
