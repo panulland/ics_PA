91f65d2b4d5e3af13c020982300db9ed25d99712
monitor <tracer@njuics.org>
2018-10-19 00:50:01 +0800

    monitor test

--- nemu/src/cpu/instr/and.c
+++ nemu/src/cpu/instr/and.c
@@ -6,7 +6,7 @@ static void instr_execute_2op() {
 	if(opr_src.data_size==8&&opr_dest.data_size==data_size)
 	{
 		opr_src.val=sign_ext(opr_src.val,8) & (0xFFFFFFFF >> (32 - data_size));
-		printf("====\n");
+		printf("====%x\n",opr_src.val);
 	}
 	opr_dest.val=alu_and(opr_src.val,opr_dest.val,opr_dest.data_size);
 	operand_write(&opr_dest);
