468c5c14cac3c0dea53fafb571bcb988d249a883
tracer-pa2018-fall <tracer@njuics.org>
2018-10-19 21:18:14 +0800

    >  nemu 171860516 cpl Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  21:18:14 up 17:53,  1 user,  load average: 0.01, 0.04, 0.07 f331da51d98eb90d2311639424d5ebb4a909e4ba

--- nemu/src/cpu/decode/opcode.c
+++ nemu/src/cpu/decode/opcode.c
@@ -85,7 +85,7 @@ instr_func group_2_b_entry[8] =
 
 /* 0xc1 */
 instr_func group_2_v_entry[8] =
-{inv, inv, inv, inv, shl_i2rm_bv, shr_i2rm_bv, inv, sar_i2rm_bv};
+{inv, inv, inv, inv, shl_i2rm_bv, shr_i2rm_bv, inv, __ref_sar_i2rm_bv};
 
 /* 0xd0 */
 instr_func group_2_1b_entry[8] =
@@ -101,7 +101,7 @@ instr_func group_2_cb_entry[8] =
 
 /* 0xd3 */
 instr_func group_2_cv_entry[8] =
-{inv, inv, inv, inv, shl_c2rm_bv, inv, shr_c2rm_bv, sar_c2rm_bv};
+{inv, inv, inv, inv, __ref_shl_c2rm_bv, inv, shr_c2rm_bv, sar_c2rm_bv};
 
 /* 0xf6 */
 instr_func group_3_b_entry[8] =
