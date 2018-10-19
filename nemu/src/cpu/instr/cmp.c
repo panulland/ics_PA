a9355c21ac4713b02167438ee58c112027547588
monitor <tracer@njuics.org>
2018-10-19 22:49:01 +0800

    monitor test

--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -5,10 +5,8 @@ static void instr_execute_2op() {
 	operand_read(&opr_dest);
 	uint32_t temp;
 	temp = opr_dest.val;
-	if(opr_src.data_size == 8 && opr_src.data_size == data_size)
-	{
+	if(opr_src.data_size == 8 && opr_dest.data_size == data_size)
 		opr_src.val = sign_ext(opr_src.val,8) & (0xFFFFFFFF >> (32 - data_size));
-	}
 	opr_dest.val = alu_sub(opr_src.val,opr_dest.val,opr_dest.data_size);
 	opr_dest.val = temp;
 }
