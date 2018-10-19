ff142be5a054af8e505da0dcb7aae66e31930f40
tracer-pa2018-fall <tracer@njuics.org>
2018-10-19 20:04:55 +0800

    >  nemu 171860516 cpl Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  20:04:55 up 16:40,  1 user,  load average: 0.11, 0.10, 0.09 9dfe83893972b542b2e5a91038524da0215ac555

--- nemu/src/cpu/decode/opcode.c
+++ nemu/src/cpu/decode/opcode.c
@@ -1,6 +1,6 @@
 #include "cpu/instr.h"
 
-instr_func __ref_opcode_entry[256] = {
+instr_func opcode_entry[256] = {
 /* 0x00 - 0x03*/	__ref_add_r2rm_b, __ref_add_r2rm_v, __ref_add_rm2r_b, __ref_add_rm2r_v,
 /* 0x04 - 0x07*/	__ref_add_i2a_b, __ref_add_i2a_v, inv, inv,
 /* 0x08 - 0x0b*/	__ref_or_r2rm_b, __ref_or_r2rm_v, __ref_or_rm2r_b, __ref_or_rm2r_v,
@@ -155,7 +155,7 @@ instr_func group_x87_df_entry[8] =
 // {inv, inv, inv, inv, inv, inv, inv, inv};
 
 
-instr_func __ref_opcode_2_byte_entry[256] = {
+instr_func opcode_2_byte_entry[256] = {
 /* 0x00 - 0x03*/        inv, group_7, inv, inv,
 /* 0x04 - 0x07*/        inv, inv, inv, inv,
 /* 0x08 - 0x0b*/        inv, inv, inv, inv,
