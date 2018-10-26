c05b3e3610af5f6e34483b2a1f8035b5cf1a3adb
tracer-pa2018-fall <tracer@njuics.org>
2018-10-26 08:59:40 +0800

    >  nemu 171860516 cpl Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  08:59:40 up 21:53,  1 user,  load average: 0.31, 0.51, 0.44 11e9bcdf4f91295d8a46094184f167a8784a406a

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -40,7 +40,7 @@ uint32_t loader() {
 			for(uint32_t i=0;i<ph->p_filesz;i++)
 			{
 				uint32_t data=*(uint32_t*)(ph->p_offset+i);
-				uint32_t data_=(uint32_t*)(ph->p_vaddr+i);
+				uint32_t data_=*(uint32_t*)(ph->p_vaddr+i);
 				data_=data;
 			}
 
