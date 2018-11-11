be9b5f8f71a0578a696dcceaa3b7677781c5a6c9
monitor <tracer@njuics.org>
2018-11-12 02:10:01 +0800

    monitor test

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -13,9 +13,14 @@ uint32_t cache_read(paddr_t paddr, size_t len) {
 	uint32_t num = (paddr << 20 >> 25) * 8;
 	uint32_t tag = paddr >> 12;
 	uint32_t addr = paddr << 27 >> 27;
+	uint32_t ret = 0;
 	for(int i=0; i < 8; i++) {
 		if(cache[num + i].tag == tag && cache[num + i].valid == 1) {
-			return cache[num + i].data[addr];
+			switch(len) {
+				case 1:return cache[num + i].data[addr];
+					   break;
+				case 2:res += cache[num + i].data[addr];
+						
 		}
 	}
 	for(int i=0; i < 8; i++) {
@@ -25,14 +30,14 @@ uint32_t cache_read(paddr_t paddr, size_t len) {
 			for(int j=0;j<32;j++) {
 				cache[num+i].data[j]=hw_mem[paddr+j];
 			}
-			return cache[num + i].data[0];
+			return hw_mem_read(paddr,len);
 		}
 	}
 	cache[num].tag=tag;
 	for(int j=0;j<32;j++) {
 		cache[num].data[j]=hw_mem[paddr+j];
 	}
-	return cache[num].data[0];
+	return hw_mem_write(paddr,len);
 }
 
 void cache_write(paddr_t paddr, size_t len, uint32_t data) {
@@ -41,7 +46,8 @@ void cache_write(paddr_t paddr, size_t len, uint32_t data) {
 	uint32_t addr = paddr << 27 >> 27;
 	for(int i=0; i < 8;i++) {
 		if(cache[num + i].tag == tag && cache[num + i].valid == 1) {
-			cache[num + i].data[addr] = data;
+			for(int j=0;j<len;j++)
+				cache[num + i].data[addr+j] = data;
 			hw_mem_write(paddr,len,data);
 			return;
 		}
