f51c983af53524fb1c871843d029af6ae5f76684
monitor <tracer@njuics.org>
2018-11-12 01:21:01 +0800

    monitor test

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -12,23 +12,27 @@ void init_cache() {
 uint32_t cache_read(paddr_t paddr, size_t len) {
 	uint32_t num = (paddr << 18 >> 26) * 8;
 	uint32_t tag = paddr >> 14;
-	//uint32_t addr = paddr << 18 >> 18;
+	uint32_t addr = paddr << 18 >> 18;
 	for(int i=0; i < 8; i++) {
 		if(cache[num + i].tag == tag && cache[num + i].valid == 1) {
-			return cache[num + i].data;
+			return cache[num + i].data[addr];
 		}
 	}
 	for(int i=0; i < 8; i++) {
 		if(cache[num + i].valid == 0) {
 			cache[num + i].valid = 1;
 			cache[num + i].tag = tag;
-			cache[num + i].data = hw_mem_read(paddr,len);
-			return cache[num + i].data;
+			for(int j=0;j<32;j++) {
+				cache[num+i].data[j]=hw_mem[addr+j];
+			}
+			return cache[num + i].data[0];
 		}
 	}
 	cache[num].tag=tag;
-	cache[num].data=hw_mem_read(paddr,len);
-	return cache[num].data;
+	for(int j=0;j<32;j++) {
+		cache[num].data[j]=hw_mem[addr+j];
+	}
+	return cache[num].data[0];
 }
 
 void cache_write(paddr_t paddr, size_t len, uint32_t data) {
