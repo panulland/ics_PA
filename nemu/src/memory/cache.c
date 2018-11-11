7bb3ba3dfbec9a89a9b125b369efd5a533d23f3c
tracer-pa2018-fall <tracer@njuics.org>
2018-11-12 00:04:47 +0800

    >  nemu 171860516 cpl Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  00:04:47 up 1 day, 13:13,  1 user,  load average: 0.04, 0.02, 0.00 b40f57d3f4c08082ab171482b7d15cc0b8a4c78

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -3,5 +3,44 @@
 void init_cache() {
 	for(int i=0;i < 1024;i++) {
 		cache[i].valid = 0;
+		cache[i].tag = 0;
+		cache[i].data = 0;
 	}
+}
+
+uint32_t cache_read(paddr_t paddr, size_t len) {
+	uint32_t num = (paddr << 11 >> 25) * 8;
+	uint32_t tag = paddr >> 21;
+	uint32_t addr = paddr << 18 >> 18;
+	for(int i=0; i < 8; i++) {
+		if(cache[num + i].tag == tag && cache[num + i].valid == 1) {
+			return cache[num + i].data;
+		}
+	}
+	for(int i=0; i < 8; i++) {
+		if(cache[num + i].valid == 0) {
+			cache[num + i].valid = 1;
+			cache[num + i].tag = tag;
+			cache[num + i].data = paddr_read(paddr,len);
+			return cache[num + i].data;
+		}
+	}
+	cache[num].tag=tag;
+	cache[num].data=paddr_read(paddr,len);
+	return cache[num].data;
+}
+
+void cache_write(paddr_t paddr, size_t len, uint32_t data) {
+	uint32_t num = (paddr << 11 >> 25) * 8;
+	uint32_t tag = paddr >> 21;
+	uint32_t addr = paddr << 18 >> 18;
+	for(int i=0; i < 8;i++) {
+		if(cache[num + i].tag == tag && cache[num + i].valid == 1) {
+			cache[num + i].data = data;
+			paddr_write(paddr,len,data);
+			return;
+		}
+	}
+	paddr_write(paddr,len,data);
+	return;
 }
