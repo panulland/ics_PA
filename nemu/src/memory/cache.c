8bc5d4977ad1db44853a3a67f6114db56eae7bf6
tracer-pa2018-fall <tracer@njuics.org>
2018-11-12 09:12:43 +0800

    >  nemu 171860516 cpl Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  09:12:43 up  2:16,  1 user,  load average: 0.10, 0.06, 0.01 84be01d91888a93af00b949b61227850169804f3

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -58,7 +58,7 @@ uint32_t cache_read(paddr_t paddr, size_t len) {
 	for(int j=0;j<32;j++) {
 		cache[num].data[j]=hw_mem[paddr-addr+j];
 	}
-	return hw_mem_write(paddr,len);
+	return hw_mem_read(paddr,len);
 }
 
 void cache_write(paddr_t paddr, size_t len, uint32_t data) {
