157a207d882880766c22068add0cec8f466e2ae2
tracer-pa2018-fall <tracer@njuics.org>
2018-11-12 09:11:49 +0800

    >  nemu 171860516 cpl Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  09:11:49 up  2:15,  1 user,  load average: 0.23, 0.07, 0.02 e243e429155f80fa81f4483b4302aee74de7139f

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -13,16 +13,34 @@ uint32_t cache_read(paddr_t paddr, size_t len) {
 	uint32_t num = (paddr << 20 >> 25) * 8;
 	uint32_t tag = paddr >> 12;
 	uint32_t addr = paddr << 27 >> 27;
-	uint32_t ret = 0;
+	if(addr+len>32) {
+		size_t len1=32-addr;
+		size_t len2=len-len1;
+		switch(len) {
+			case 2:return (cache_read(paddr,1)<<8) + cache_read(paddr+1,1);
+			case 4:return (cache_read(paddr,len1)<<len2) + cache_read(paddr+len1,len2);
+		}
+	}
+	uint32_t res = 0;
 	for(int i=0; i < 8; i++) {
 		if(cache[num + i].tag == tag && cache[num + i].valid == 1) {
 			switch(len) {
 				case 1:return cache[num + i].data[addr];
 				       break;
-				case 2:res += cache[num + i].data[addr];
-				       res += cache[num + i].data[addr + 1]<<8;
+				case 2:res += cache[num + i].data[addr]<<8;
+				       res += cache[num + i].data[addr + 1];
+				       return res;
 				       break;
-				case 4:res += cache[num + i].
+				case 3:res += cache[num + i].data[addr]<<16;
+				       res += cache[num + i].data[addr+1]<<8;
+				       res += cache[num + i].data[addr+2];
+				case 4:res += cache[num + i].data[addr]<<24;
+				       res += cache[num + i].data[addr + 1]<<16;
+				       res += cache[num + i].data[addr + 2]<<8;
+				       res += cache[num + i].data[addr + 3];
+				       return res;
+				       break;
+			}
 						
 		}
 	}
@@ -47,10 +65,22 @@ void cache_write(paddr_t paddr, size_t len, uint32_t data) {
 	uint32_t num = (paddr << 20 >> 25) * 8;
 	uint32_t tag = paddr >> 12;
 	uint32_t addr = paddr << 27 >> 27;
+	if(addr+len>32) {
+		size_t len1=32-addr;
+		size_t len2=len-len1;
+		switch(len) {
+			case 2:cache_write(paddr,1,data>>8);
+			       cache_write(paddr+1,1,data<<24>>24);
+			       return;
+			case 4:cache_write(paddr,len1,data>>(len2*8));
+			       cache_write(paddr,len2,data<<(32-len2*8)>>(32-len2*8));
+			       return;
+		}
+	}
 	for(int i=0; i < 8;i++) {
 		if(cache[num + i].tag == tag && cache[num + i].valid == 1) {
-			for(int j=0;j<len;j++)
-				cache[num + i].data[addr+j] = data;
+			for(int j=0;j<len;j++) {
+				cache[num + i].data[addr+j] = data >> (8*(len - 1 -j));
 			hw_mem_write(paddr,len,data);
 			return;
 		}
