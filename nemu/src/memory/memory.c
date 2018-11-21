521c4752c79041605a5e55763cd8fcbf3d04e9f0
tracer-pa2018-fall <tracer@njuics.org>
2018-11-21 23:49:24 +0800

    >  nemu 171860516 cpl Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  23:49:24 up 14:13,  1 user,  load average: 0.13, 0.17, 0.17 311a9e37458a58c34e4b5f896881a7526ee731ad

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -54,6 +54,7 @@ uint32_t vaddr_read(vaddr_t vaddr, uint8_t sreg, size_t len) {
 #else
 	uint32_t laddr = vaddr;
 	if(cpu.cr0.pe == 1) {
+		printf("==========\n");
 		laddr = segment_translate(vaddr, sreg);
 	}
 	return laddr_read(laddr, len);
@@ -67,7 +68,6 @@ void vaddr_write(vaddr_t vaddr, uint8_t sreg, size_t len, uint32_t data) {
 #else
 	uint32_t laddr = vaddr;
 	if(cpu.cr0.pe == 1) {
-		printf("==========\n");
 		laddr = segment_translate(vaddr, sreg);
 	}
 	laddr_write(laddr, len, data);
