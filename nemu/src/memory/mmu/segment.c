dd6b9b73c3fbf74c0d02987fc04b14b26883bb38
monitor <tracer@njuics.org>
2018-11-21 23:27:01 +0800

    monitor test

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -13,7 +13,7 @@ uint32_t segment_translate(uint32_t offset, uint8_t sreg) {
 
 // load the invisible part of a segment register
 void load_sreg(uint8_t sreg) {
-	SegDesc *s = cpu.segReg[sreg].index * 8 + cpu.gdtr.base;
+	SegDesc s = cpu.segReg[sreg].index * 8 + cpu.gdtr.base;
 	cpu.segReg[sreg].base = (s->base_15_0) + (s->base_23_16 << 16) + (s->base_31_24 << 24);
 	cpu.segReg[sreg].limit = (s->limit_15_0) + (s->limit_19_16 << 16);
 	/* TODO: load the invisibile part of the segment register 'sreg' by reading the GDT.
