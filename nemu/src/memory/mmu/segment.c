14ca1efdc266b220f6af5097dfe91194b42c4318
monitor <tracer@njuics.org>
2018-11-22 23:07:01 +0800

    monitor test

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -17,6 +17,9 @@ void load_sreg(uint8_t sreg) {
 	memcpy(&s.val,hw_mem + cpu.gdtr.base + cpu.segReg[sreg].index * 8, 8);
 	cpu.segReg[sreg].base = (s.base_15_0) + (s.base_23_16 << 16) + (s.base_31_24 << 24);
 	cpu.segReg[sreg].limit = (s.limit_15_0) + (s.limit_19_16 << 16);
+	cpu.segReg[sreg].type = s.type;
+	cpu.segReg[sreg].soft_use = s.soft_use;
+	cpu.segReg[sreg].privilege_level = s.privilege_level;
 	/* TODO: load the invisibile part of the segment register 'sreg' by reading the GDT.
 	 * The visible part of 'sreg' should be assigned by mov or ljmp already.
 	 */
