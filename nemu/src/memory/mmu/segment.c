d997e6ae30af0a2c3591b48bbada6ddae0c34caa
tracer-pa2018-fall <tracer@njuics.org>
2018-11-23 00:21:00 +0800

    >  nemu 171860516 cpl Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  00:21:00 up 15:57,  1 user,  load average: 0.23, 0.25, 0.29 90cc6c7fe52df302f8949e70a4486093d4336a5

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -17,7 +17,7 @@ void load_sreg(uint8_t sreg) {
 	s.val[0] = s.val[1] = 0;
 	memcpy(&s, hw_mem + cpu.gdtr.base + cpu.segReg[sreg].index * 8, 8);
 	printf("%x %x\n",s.val[0],s.val[1]);
-	printf("%x %x %x\n",s.base_15_0,s.limit_15_0,s.base_31_24);
+	printf("%x %x %x %x\n",s.limit_15_0,s.base_15_0,s.type,s.base_31_24);
 	cpu.segReg[sreg].base = (s.base_15_0) + (s.base_23_16 << 16) + (s.base_31_24 << 24);
 	cpu.segReg[sreg].limit = (s.limit_15_0) + (s.limit_19_16 << 16);
 	cpu.segReg[sreg].type = s.type;
