d9fc8801a2255a689fb7f9d03fbb6726c692ba65
monitor <tracer@njuics.org>
2018-11-23 00:18:01 +0800

    monitor test

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -15,10 +15,9 @@ uint32_t segment_translate(uint32_t offset, uint8_t sreg) {
 void load_sreg(uint8_t sreg) {
 	SegDesc s;
 	s.val[0] = s.val[1] = 0;
-	printf("%x %x\n",s.val[0],s.val[1]);
 	memcpy(&s, hw_mem + cpu.gdtr.base + cpu.segReg[sreg].index * 8, 8);
 	printf("%x %x\n",s.val[0],s.val[1]);
-	printf("%x %x %x\n",s.base_15_0,s.base_23_16,s.base_31_24);
+	printf("%x %x %x\n",s.base_15_0,s.limit_15_0,s.base_31_24);
 	cpu.segReg[sreg].base = (s.base_15_0) + (s.base_23_16 << 16) + (s.base_31_24 << 24);
 	cpu.segReg[sreg].limit = (s.limit_15_0) + (s.limit_19_16 << 16);
 	cpu.segReg[sreg].type = s.type;
