076db0879f09e503d82b6b3996f4548aa9871730
monitor <tracer@njuics.org>
2018-11-22 23:48:01 +0800

    monitor test

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -18,7 +18,7 @@ void load_sreg(uint8_t sreg) {
 	printf("+++++++++++\n");
 	printf("%x %x %x\n",cpu.gdtr.base,cpu.gdtr.limit,cpu.segReg[sreg].index);
 	memcpy(&s.val,hw_mem + cpu.gdtr.base + cpu.segReg[sreg].index * 8, 8);
-	printf("%x\n",s.val[0]);
+	printf("%x\n",s.val);
 	cpu.segReg[sreg].base = (s.base_15_0) + (s.base_23_16 << 16) + (s.base_31_24 << 24);
 	cpu.segReg[sreg].limit = (s.limit_15_0) + (s.limit_19_16 << 16);
 	cpu.segReg[sreg].type = s.type;
