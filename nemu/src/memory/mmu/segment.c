4ccbdc57c734c431ece8a3d4de2a55b5e20d248a
tracer-pa2018-fall <tracer@njuics.org>
2018-11-24 16:43:48 +0800

    >  nemu 171860516 cpl Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  16:43:48 up 17:02,  1 user,  load average: 0.54, 0.33, 0.18 4850c696bb1ecfa64167e2759a757e3e3a16fc07

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -14,23 +14,12 @@ uint32_t segment_translate(uint32_t offset, uint8_t sreg) {
 // load the invisible part of a segment register
 void load_sreg(uint8_t sreg) {
 	SegDesc s;
-	//s.val[0] = s.val[1] = 0;
 	memcpy(&s, hw_mem + cpu.gdtr.base + cpu.segReg[sreg].index * 8, 8);
-	//printf("%x %x\n",s.val[0],s.val[1]);
-	//printf("%x %x %x %x\n",s.limit_15_0,s.base_15_0,s.type,s.base_31_24);
 	cpu.segReg[sreg].base = (s.base_15_0) + (s.base_23_16 << 16) + (s.base_31_24 << 24);
 	cpu.segReg[sreg].limit = (s.limit_15_0) + (s.limit_19_16 << 16);
 	cpu.segReg[sreg].type = s.type;
 	cpu.segReg[sreg].soft_use = s.soft_use;
 	cpu.segReg[sreg].privilege_level = s.privilege_level;
-	//printf("%x\n",cpu.segReg[sreg].base);
-	/*if(cpu.segReg[sreg].base !=0) {
-		assert(cpu.segReg[sreg].base != 0);
-	}
-	if(cpu.segReg[sreg].limit != 0xfffff)
-	assert(cpu.segReg[sreg].limit != 0xfffff);
-	if(s.granularity != 0)
-	assert(s.granularity!=0);*/
 	/* TODO: load the invisibile part of the segment register 'sreg' by reading the GDT.
 	 * The visible part of 'sreg' should be assigned by mov or ljmp already.
 	 */
