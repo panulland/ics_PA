9dc477c0f2b05746f2273c1474c9f0e8b0a89227
tracer-pa2018-fall <tracer@njuics.org>
2018-12-04 00:17:28 +0800

    >  nemu 171860516 cpl Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  00:17:28 up 20:13,  1 user,  load average: 0.13, 0.11, 0.14 f9193cc9392a70761f85f4941df1d048e948af59

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -4,8 +4,18 @@
 // translate from linear address to physical address
 paddr_t page_translate(laddr_t laddr) {
 #ifndef TLB_ENABLED
-	printf("\nPlease implement page_translate()\n");
-	assert(0);
+	//printf("\nPlease implement page_translate()\n");
+	//assert(0);
+	uint32_t dir = laddr >> 22;
+	uint32_t page = laddr << 10 >> 22;
+	uint32_t offset = laddr << 20 >> 20;
+	PDE pde;
+	PTE pte;
+	memcpy(&pde.val, hw_mem + (cpu.cr3.pdbr << 12 + dir * 4), 4);
+	assert(pde.present == 1);
+	memcpy(&pte.val, hw_mem + (pde.page_frame < 12) + page * 4, 4);
+	assert(pte.present == 1);
+	return (pte.page_frame << 12) + offset;
 #else	
 	return tlb_read(laddr) | (laddr & PAGE_MASK);;
 #endif
