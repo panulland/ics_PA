6e394e10a9c90ed0029edebf74405ba40bc58055
monitor <tracer@njuics.org>
2018-12-03 23:40:01 +0800

    monitor test

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -39,7 +39,20 @@ void paddr_write(paddr_t paddr, size_t len, uint32_t data) {
 
 
 uint32_t laddr_read(laddr_t laddr, size_t len) {
-	return paddr_read(laddr, len);
+	assert(len == 1 || len == 2 || len == 4);
+	if(cpu.cr0.pg == 1) {
+		if () {
+			/* this is a special case, you can handle it later. */
+			assert(0);
+			} 
+		else {
+			uint32_t paddr = page_translate(laddr);
+			return paddr_read(paddr, len);
+		}
+	} 
+	else { 
+
+	}
 }
 
 void laddr_write(laddr_t laddr, size_t len, uint32_t data) {
