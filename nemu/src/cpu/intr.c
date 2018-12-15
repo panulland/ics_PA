0665b6ef37aa82521eb5e59c12602645858fd044
monitor <tracer@njuics.org>
2018-12-15 23:47:01 +0800

    monitor test

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -28,9 +28,7 @@ void raise_intr(uint8_t intr_no) {
 	if(gate.type == 0xe) {
 		cpu.eflags.IF = 0;
 	}
-	printf("%x\n",cpu.eip);
 	cpu.eip = gate.offset_15_0 + (gate.offset_31_16 << 16);
-	printf("%x\n",cpu.eip);
 #endif
 }
 
