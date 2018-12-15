bf9b325b81f2b623f58d2e14b9dcedee035bdd2c
monitor <tracer@njuics.org>
2018-12-15 10:44:01 +0800

    monitor test

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -6,7 +6,7 @@ make_instr_func(lgdt) {
     int len = 1;
     len += modrm_rm(eip+1,&opr_src);
     operand_read(&opr_src);
-    printf("%x %x %x\n",opr_src.val,opr_src.mem_addr.base,opr_src.mem_addr.disp);
+    printf("%x %x %x %x %x\n",opr_src.val,opr_src.mem_addr.base,opr_src.mem_addr.disp,opr_src.mem_addr.index,opr_src.mem_addr.scale);
     print_asm_1("lgdt","",len + 1,&opr_src);
     return len; 
 }
