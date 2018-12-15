78e02e5a67c95c692692db9a7418721f530cdbfa
monitor <tracer@njuics.org>
2018-12-15 10:03:01 +0800

    monitor test

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -1,7 +1,11 @@
 #include "cpu/instr.h"
 
 make_instr_func(lgdt) {
-    OPERAND opr_src;
+    OPERAND opr_src,rm;
+    int len = 1;
+    len += modrm_rm(eip,&rm);
+    operand_read(&rm);
+    printf("%x\n",rm.val);
     opr_src.data_size=data_size;
     opr_src.type = OPR_IMM; 
 	opr_src.sreg = SREG_CS; 
