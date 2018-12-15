917dd69fbe5707f3692f9dee83b32ccb6da67d6b
tracer-pa2018-fall <tracer@njuics.org>
2018-12-15 21:40:46 +0800

    >  nemu 171860516 cpl Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  21:40:46 up 1 day, 12:19,  1 user,  load average: 0.47, 0.29, 0.27 1295afdd82ddbf1e4b0748fc48ff0022c8c16615

--- nemu/src/cpu/instr/if.c
+++ nemu/src/cpu/instr/if.c
@@ -2,12 +2,12 @@
 
 make_instr_func(cli) {
     cpu.eflags.IF = 0;
-    print_asm_0("cli",,1);
+    print_asm_0("cli","",1);
     return 1;
 }
 
 make_instr_func(sti) {
     cpu.eflags.IF = 1;
-    print_asm_0("sti",,1);
+    print_asm_0("sti","",1);
     return 1;
 }
