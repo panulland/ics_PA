544b6869ffa21cadaa15b339c7db569da20394d7
tracer-pa2018-fall <tracer@njuics.org>
2018-12-15 13:42:28 +0800

    >  nemu 171860516 cpl Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  13:42:28 up 1 day, 11:27,  1 user,  load average: 0.98, 0.72, 0.60 169ec2a2af55f5483916f018cab1fc84a991294f

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -6,6 +6,7 @@
 void raise_intr(uint8_t intr_no) {
 #ifdef IA32_INTR
 	GateDesc gate;
+	gate.val[0] = gate.val[1] = 0;
 	printf("%x %x\n",cpu.idtr.base,cpu.idtr.limit);
 	uint32_t index = intr_no;
 	printf("%x\n",index* 8);
