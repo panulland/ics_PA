5f50649a143d1f8a2cb64fa3a3ba6d800fd8bb72
tracer-pa2018-fall <tracer@njuics.org>
2018-12-15 13:43:57 +0800

    >  nemu 171860516 cpl Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  13:43:57 up 1 day, 11:28,  1 user,  load average: 1.08, 0.80, 0.63 355024ac2c36d82b4a9c6e3988aab8da5a225dd

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -11,7 +11,7 @@ void raise_intr(uint8_t intr_no) {
 	uint32_t index = intr_no;
 	printf("%x\n",index);
 	printf("%x %x\n",gate.val[0],gate.val[1]);
-	memcpy(&gate, hw_mem + cpu.idtr.base + index * 8, 8);
+	memcpy(&gate.val, hw_mem + cpu.idtr.base + index * 8, 8);
 	cpu.esp -= 4;
 	OPERAND temp;
 	temp.data_size = data_size;
