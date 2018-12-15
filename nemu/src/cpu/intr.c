22aa3b3b8d54c2f3638b6c5f7d12fdd61ccd4f6d
tracer-pa2018-fall <tracer@njuics.org>
2018-12-15 13:39:24 +0800

    >  nemu 171860516 cpl Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  13:39:24 up 1 day, 11:24,  1 user,  load average: 1.08, 0.76, 0.59 696367c19ee090f79d2b49a2e9f4f31f356f6e31

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -26,6 +26,7 @@ void raise_intr(uint8_t intr_no) {
 	temp.val = cpu.eip;
 	operand_write(&temp);
 	cpu.eip = gate.offset_15_0 + (gate.offset_31_16 << 16);
+	assert(0);
 	printf("%x\n",cpu.eip);
 	assert(0);
 #endif
