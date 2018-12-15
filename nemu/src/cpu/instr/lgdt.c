674f1d045ae06520febbeb43568c6d386b27f90c
tracer-pa2018-fall <tracer@njuics.org>
2018-12-15 10:05:38 +0800

    >  nemu 171860516 cpl Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  10:05:38 up 1 day,  8:38,  1 user,  load average: 0.18, 0.18, 0.20 aaa71d90b6bb403b175facf72c56870040da59f3

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -4,7 +4,7 @@ make_instr_func(lgdt) {
     OPERAND opr_src,rm;
     rm.data_size=32;
     int len = 1;
-    len += modrm_rm(eip,&rm);
+    len += modrm_rm(eip+1,&rm);
     operand_read(&rm);
     printf("%x\n",rm.val);
     opr_src.data_size=data_size;
