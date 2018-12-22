b4d98aef4d02a51e984e9e35c385a43b4e9ec713
tracer-pa2018-fall <tracer@njuics.org>
2018-12-22 15:50:52 +0800

    >  nemu 171860516 cpl Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  15:50:52 up 1 day, 18:11,  1 user,  load average: 0.49, 0.33, 0.26 d31131c5d811eec7656ef1d48624f96149af788d

---
+++ nemu/src/cpu/instr/io.c
@@ -0,0 +1,15 @@
+#include "cpu/instr.h"
+
+make_instr_func(in_b) {
+    OPERAND al,dx;
+    al.data_size = dx.data_size = 8;
+    al.type = OPR_REG;
+    dx.type = OPR_REG;
+    al.addr = REG_AL;
+    dx.addr = REG_DX;
+    operand_read(&dx);
+    al.val = pio_read(dx.val,1);
+    operand_write(&al);
+    print_asm0("in","",&dx);
+    return 1;
+}
