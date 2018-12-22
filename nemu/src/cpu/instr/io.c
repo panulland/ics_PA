f1c7c02ba2fb219248fcac65b455e0fcacac6d7e
monitor <tracer@njuics.org>
2018-12-22 15:58:01 +0800

    monitor test

--- nemu/src/cpu/instr/io.c
+++ nemu/src/cpu/instr/io.c
@@ -15,3 +15,18 @@ make_instr_func(in_b) {
     operand_write(&al);
     return 1;
 }
+
+make_instr_func(in_v) {
+    OPERAND al,dx;
+    al.data_size = 8;
+    dx.data_size = 16;
+    al.type = OPR_REG;
+    dx.type = OPR_REG;
+    al.addr = REG_AL;
+    dx.addr = REG_DX;
+    operand_read(&dx);
+    print_asm_1("in","",1,&dx);
+    al.val = pio_read(dx.val,1);
+    operand_write(&al);
+    return 1;
+}
