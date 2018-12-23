986779e54d1dd84df7e5ec7baf05f6370501b6b0
tracer-pa2018-fall <tracer@njuics.org>
2018-12-23 20:21:06 +0800

    >  nemu 171860516 cpl Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  20:21:06 up 1 day, 21:56,  1 user,  load average: 0.57, 0.37, 0.31 4461c30ebe462bf37c4b4f46ecd9caf76d3a431b

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -13,10 +13,6 @@ paddr_t page_translate(laddr_t laddr) {
 	PTE pte;
 	pde.val = paddr_read((cpu.cr3.pdbr << 12) + dir * 4, 4);
 	//memcpy(&pde.val, hw_mem + ((cpu.cr3.pdbr << 12) + dir * 4), 4);
-	if(laddr == 0xa0000) {
-		printf("%x\n",pde.val);
-	}
-	assert(laddr != 0xa0000);
 	if(pde.present == 0)
 	{
 		printf("%x %x\n",pde.val, laddr);
@@ -29,6 +25,9 @@ paddr_t page_translate(laddr_t laddr) {
 		printf("%x %x\n",pte.val, laddr);
 	}
 	assert(pte.present == 1);
+	if(laddr == 0xa0000) {
+		printf("%x %x\n",pde.val,pte.val);
+	}
 	return (pte.page_frame << 12) + offset;
 #else	
 	return tlb_read(laddr) | (laddr & PAGE_MASK);;
