b8b93860eb77c978a5a976ee0dc76b7c4c3ec6c7
monitor <tracer@njuics.org>
2018-12-23 21:38:01 +0800

    monitor test

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -13,20 +13,9 @@ paddr_t page_translate(laddr_t laddr) {
 	PTE pte;
 	pde.val = paddr_read((cpu.cr3.pdbr << 12) + dir * 4, 4);
 	//memcpy(&pde.val, hw_mem + ((cpu.cr3.pdbr << 12) + dir * 4), 4);
-	if(pde.present == 0)
-	{
-		printf("%x %x\n",pde.val, laddr);
-	}
 	assert(pde.present == 1);
 	pte.val = paddr_read((pde.page_frame << 12) + page * 4, 4);
 	//memcpy(&pte.val, hw_mem + ((pde.page_frame << 12) + page * 4), 4);
-	if(pte.present == 0)
-	{
-		printf("%x %x %x\n",pde.val, pte.val, laddr);
-	}
-	if(laddr > 0xa0000 && laddr < 0xa0000+320*200) {
-		printf("=========%x %x\n",laddr,pte.page_frame);
-	}
 	assert(pte.present == 1);
 	return (pte.page_frame << 12) + offset;
 #else	
