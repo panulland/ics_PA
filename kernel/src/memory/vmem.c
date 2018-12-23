95d2c4a25fc8548e3a6c434a5942996e1a13b2b7
tracer-pa2018-fall <tracer@njuics.org>
2018-12-23 23:14:51 +0800

    >  nemu 171860516 cpl Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  23:14:51 up 2 days, 49 min,  1 user,  load average: 0.28, 0.32, 0.27 6cbc18edfdded24cf86dc99cae49ba15394ac32a

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -21,6 +21,14 @@ void create_video_mapping() {
 	//panic("please implement me");
 	PDE *pdir = get_updir();
 	pdir->present = 1;
+	PTE *ptable = (PTE *)((uint32_t)pdir->page_frame);
+	for(uint32_t i = 0; i < NR_PT; i++) {
+		uint32_t page = 0xa0 + i;
+		PTE* pte;
+		pte = ptable + page;
+		pte->present = 1;
+		pte->page_frame = page;
+	}
 	pdir += KOFFSET / PT_SIZE;
 	pdir->present = 1;
 	PTE *ptable = (PTE *)((uint32_t)pdir->page_frame);
