325c85664bb72f8659269f755e1bb5dc3c77a5c8
tracer-pa2018-fall <tracer@njuics.org>
2019-01-18 13:50:56 +0800

    >  nemu 171860516 cpl Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  13:50:56 up 2 days,  6:39,  1 user,  load average: 0.59, 0.39, 0.23 6045a889b59174cfdee41abb7e7100026144862

--- kernel/src/fs/fs.c
+++ kernel/src/fs/fs.c
@@ -43,9 +43,9 @@ void ide_write(uint8_t *, uint32_t, uint32_t);
 int fs_open(const char *pathname, int flags) {
 	//panic("Please implement fs_open at fs.c");
 	//return -1;
-	Log("%d\n",sizeof(file_table));
+	Log("%d\n",(sizeof(file_table)/sizeof(file_info));
 	assert(0);
-	for(int i=0;strcmp(file_table[i].name,pathname)&&i<sizeof(file_table);i++);
+	for(int i=0;strcmp(file_table[i].name,pathname)&&i<(sizeof(file_table)/sizeof(file_info));i++);
 	return -1;
 }
 
