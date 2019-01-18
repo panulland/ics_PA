358bea329409e0f3dcd5a9bd449904e5e0986f65
tracer-pa2018-fall <tracer@njuics.org>
2019-01-18 14:01:59 +0800

    >  nemu 171860516 cpl Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  14:01:59 up 2 days,  6:50,  1 user,  load average: 0.55, 0.44, 0.34 ea86b82b2aeccd37878b6cf8c0f2120ff9ffa0a8

--- kernel/src/fs/fs.c
+++ kernel/src/fs/fs.c
@@ -77,6 +77,7 @@ off_t fs_lseek(int fd, off_t offset, int whence) {
 }
 
 int fs_close(int fd) {
-	panic("Please implement fs_close at fs.c");
-	return -1;
+	//panic("Please implement fs_close at fs.c");
+	//return -1;
+	files[fd].used = false;
 }
